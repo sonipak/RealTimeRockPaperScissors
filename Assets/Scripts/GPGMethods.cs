dcd5c4789cd8b91d3108c0ce3c1131f3bb5402f7
sonipak <wowaaa347@gmail.com>
2018-10-09 20:39:12 -0500

    Initial commit

---
+++ Assets/Scripts/GPGMethods.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi.Multiplayer;
+using System.Threading.Tasks;
+using System;
+
+public class GPGMethods : MonoBehaviour, RealTimeMultiplayerListener {
+	public bool Initialized{ get; private set; }
+	public bool SignedIn{ get; private set;}
+
+
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder ().Build ();
+		PlayGamesPlatform.InitializeInstance (config);
+		PlayGamesPlatform.Activate ();
+
+		Initialized = true;
+	}
+
+	public async Task<bool> SignInAsync(){
+		if (Initialized) {
+			Social.Active.Authenticate (Social.localUser, (bool success) => {
+				if (success) {
+					SignedIn = true;
+					return success;
+
+				} else {
+					Debug.Log ("GPG Login Failed");
+				}
+			});
+		}else {
+			Debug.Log ("Social platform has not been initialized yet");
+		}
+	}
+
+	public void QueueRandomMatch(uint minimumPlayers, uint maxPlayers, uint gameVariant){
+		if (SignedIn) {
+			PlayGamesPlatform.Instance.RealTime.CreateQuickGame (minimumPlayers, maxPlayers, gameVariant, this);
+		} else {
+			Debug.Log ("User is not signed into GPG");
+		}
+	}
+
+	public void OnRoomSetupProgress(float percent){
+
+	}
+	public void OnRoomConnected(bool success){
+		 
+	}
+	public void OnLeftRoom(){
+	
+	}
+	public void OnParticipantLeft(Participant participant){
+
+	}
+	public void OnPeersConnected(string[] participantIds){
+
+	}
+	public void OnPeersDisconnected(string[] participantIds){
+
+	}
+	public void OnRealTimeMessageReceived(bool isReliable, string senderId, byte[] data){
+
+	}
+}
---
+++ Assets/Scripts/QueueButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi.Multiplayer;
+
+
+public class QueueButton : MonoBehaviour {
+	const int MIN_OPPONENTS = 1, MAX_OPPONENTS = 1;
+	const int gameVariant = 0;
+
+	[SerializeField]
+	GameObject GPGObject;
+	GPGMethods GPGScripts;
+	[SerializeField]
+	Text text;
+	Button button;
+
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<Button> ();
+		button.onClick.AddListener (clickedButton);
+		GPGScripts = GPGObject.GetComponent<GPGMethods> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void clickedButton(){
+		button.enabled = false;
+		text.text = "Signing in...";
+		if(GPGScripts.SignInAsync())
+			text.text = "Signed in!";
+	}
+}
